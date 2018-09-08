761b6bbd20b261ad2d47747551dfbe424ac0e6ee
Mark Watson <markw@markwatson.com>
2018-09-08 09:45:24 -0700

    added dropout

---
+++ Makefile
@@ -0,0 +1,3 @@
+clean:
+	rm -f b*.csv w*.csv
+
--- cancer_trainer.py
+++ cancer_trainer.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Dropout
 from keras.models import Model
 import numpy as np
 import pprint as pp
@@ -16,17 +16,22 @@ X_test = X_test[:,0:-1]
 
 inputs = Input(shape=[9])
 
-x = Dense(64, activation='relu')(inputs)
-x = Dense(64, activation='relu')(x)
+x = Dense(64, activation='relu', use_bias=False)(inputs)
+x = Dropout(0.3)(x)
+x = Dense(64, activation='relu', use_bias=False)(x)
+x = Dropout(0.3)(x)
 
-probs = Dense(1, activation='sigmoid')(x)
+probs = Dense(1, activation='sigmoid', use_bias=False)(x)
 
 
 model1 = Model(inputs=inputs, outputs=probs)
-model1.compile(optimizer='sgd', loss='binary_crossentropy')
+model1.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
 
-model1.fit(X_train, Y_train, epochs=100, batch_size=128,
+model1.fit(X_train, Y_train, epochs=10000, batch_size=128,
            validation_split=0.15, verbose=True)
+score, acc = model1.evaluate(X_test, Y_test)
+print('Test score:', score)
+print('Test accuracy:', acc)
 
 # save model
 with open('model.json', "w") as model_save_file:
@@ -35,15 +40,15 @@ with open('model.json', "w") as model_save_file:
 
 # weights:
 weights = model1.get_weights() # as numpy arrays
-pp.pprint(weights)
+#pp.pprint(weights)
 for w in weights:
    print("w.shape=", w.shape)
 
 np.savetxt('w1.csv', weights[0], delimiter=",")
-np.savetxt('b1.csv', weights[1], delimiter=",")
+#np.savetxt('b1.csv', weights[1], delimiter=",")
 
-np.savetxt('w2.csv', weights[2], delimiter=",")
-np.savetxt('b2.csv', weights[3], delimiter=",")
+np.savetxt('w2.csv', weights[1], delimiter=",")
+#np.savetxt('b2.csv', weights[3], delimiter=",")
 
-np.savetxt('w3.csv', weights[4], delimiter=",")
-np.savetxt('b3.csv', weights[5], delimiter=",")
+np.savetxt('w3.csv', weights[2], delimiter=",")
+#np.savetxt('b3.csv', weights[5], delimiter=",")
